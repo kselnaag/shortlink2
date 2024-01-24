541d18ab72c8acd1871ff702391d827e464c165d
kselnaag <kselnaag@gmail.com>
2024-01-25 01:11:10 +0300

    add all to app constructor

--- internal/app.go
+++ internal/app.go
@@ -3,11 +3,11 @@ package app
 import (
 	"os"
 	C "shortlink2/internal/cfg"
+	D "shortlink2/internal/db"
 	H "shortlink2/internal/http"
 	L "shortlink2/internal/log"
+	S "shortlink2/internal/service"
 	T "shortlink2/internal/types"
-	// D "shortlink2/internal/db"
-	// S "shortlink2/internal/service"
 )
 
 type App struct {
@@ -20,9 +20,10 @@ type App struct {
 func NewApp() *App {
 	cfg := C.NewCfg(os.Args[0] + ".env")
 	log := L.NewLogFprintf(cfg)
-	db := D.NewSQLite(cfg, log)
-	svcsl := S.NewSvcShortLink(db, log)
-	hsrv := H.NewHTTPServerNet(svcsl, log, cfg)
+	// db := D.NewSQLite(cfg, log)
+	db := D.NewDBMock(cfg, log)
+	svcsl2 := S.NewSvcShortLink2(db, log)
+	hsrv := H.NewHTTPServerNet(svcsl2, log, cfg)
 	return &App{
 		hsrv: hsrv,
 		db:   db,
